96a97b1829b158e5a7801b1a02425b7d7f93ba4a
sabnira <sabnirajeemi@gmail.com>
2025-07-06 17:08:09 +0600

    commit

---
+++ src/pages/Coffees.jsx
@@ -0,0 +1,11 @@
+
+
+const Coffees = () => {
+    return (
+        <div>
+            coffee page
+        </div>
+    );
+};
+
+export default Coffees;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* Banner */}
+            {/* Heading */}
+            {/* Categories tab section */}
+            {/* Dynamic Nested Component */}
+        </div>
+    );
+};
+
+export default Home;
